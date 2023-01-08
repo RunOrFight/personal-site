2834e907404b35444329dc0d0f85975896bd638d
RunOrFight <dragoinfury@gmail.com>
2023-01-08 23:13:46 +0300

    [feat][frontend]: add assets

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { images } from "../../constants";
 
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img
+          src={images.logo}
+          alt='Alexander Mikhnyuk'
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

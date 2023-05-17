cefaa1c7489b8f396c7578c40adae3dcb7c5705d
RunOrFight <dragoinfury@gmail.com>
2023-05-17 13:59:45 +0300

    init: start project using vite and react-ts template

--- backend/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'lsu5bb06',
-    dataset: 'production'
-  }
-})
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className='app__container'>
+            <header></header>
+            <main>The site is under development</main>
+        </div>
+    );
+}
+
+export default App;
